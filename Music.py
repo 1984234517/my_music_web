db92a6fefb4a58dd03db4b8154a1270c3e1fe1a0
1984234517 <1984234517@qq.com>
2019-06-17 20:10:27 +0800

    更新说明

--- Music.py
+++ Music.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,jsonify
 from decoder2 import main
 import json
-
+from qq_music import get_song
 app = Flask(__name__)
 
 
@@ -14,15 +14,23 @@ def hello_world():
 def search():
     key_vlaue = request.args.get('query_value')
     print("我的参数是")
-    check_vlaue = request.form.get('cars')
+    # check_vlaue = request.values.get("selectid")
+    check_vlaue = request.args.get("type")
+    print("选择的播放器是0")
     print(check_vlaue)
-    print("wode shi ",key_vlaue)
-    song_id = main(key_vlaue)
-    print(song_id)
-    if song_id != '':
-        data=dict(data = song_id)
-        return jsonify(data)
+    print("wode shi ", key_vlaue)
+    if check_vlaue == 'qq音乐':
+        song_url = get_song(key_vlaue)
+        if song_url != '':
+            data = dict(data = song_url,statu = 0)
+            return jsonify(data)
     else:
-        return jsonify({})
+        song_id = main(key_vlaue)
+        print(song_id)
+        if song_id != '':
+            data=dict(data = song_id,statu = 1)
+            return jsonify(data)
+        else:
+            return jsonify({})
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
